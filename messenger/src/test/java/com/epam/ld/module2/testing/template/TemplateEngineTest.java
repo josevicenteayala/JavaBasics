3d5b37c203cc01de20878c6e2a1d9db1521be0a4
Jose Vicente Ayala Luna <jayala@starbucks.com>
2023-06-01 00:16:56 -0500

    Add test to validate error replacing placeholders

--- messenger/src/test/java/com/epam/ld/module2/testing/template/TemplateEngineTest.java
+++ messenger/src/test/java/com/epam/ld/module2/testing/template/TemplateEngineTest.java
@@ -1,6 +1,7 @@
 package com.epam.ld.module2.testing.template;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import com.epam.ld.module2.testing.exceptions.MissingValueException;
 import org.junit.jupiter.api.Test;
@@ -16,6 +17,14 @@ public class TemplateEngineTest {
         Map<String, String> variables = Map.of("name","Jose", "language","Java") ;
         String message = templateEngine.generateMessage(template, variables);
         assertEquals("Hello, Jose! You are a Java programmer.", message);
+    }
+
+    @Test
+    public void testGenerateMessageWithIncompleteVariablesReplacements() throws MissingValueException {
+        TemplateEngine templateEngine = new TemplateEngine();
+        Template template = new Template("Hello, #{name}! You are a #{language} programmer at #{company}.");
+        Map<String, String> variables = Map.of("name","Jose", "language","Java") ;
+        assertThrows(MissingValueException.class, () -> templateEngine.generateMessage(template, variables));
     }
 
 }
