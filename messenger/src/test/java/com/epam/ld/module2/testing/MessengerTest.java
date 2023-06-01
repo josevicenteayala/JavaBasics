c35297f0ae6110c8a9cb4d183bf4993c2619693d
Jose Vicente Ayala Luna <jayala@starbucks.com>
2023-06-01 08:02:10 -0500

    Introduce test case for send message behavior with mocked MailServer

---
+++ messenger/src/test/java/com/epam/ld/module2/testing/MessengerTest.java
@@ -0,0 +1,29 @@
+package com.epam.ld.module2.testing;
+
+import static org.mockito.Mockito.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+import com.epam.ld.module2.testing.template.Template;
+import com.epam.ld.module2.testing.template.TemplateEngine;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+
+class MessengerTest {
+
+    @Test
+    public void testSendMessage() {
+        MailServer mailServer = mock(MailServer.class);
+        TemplateEngine engine = new TemplateEngine();
+        Messenger messenger = new Messenger(mailServer, engine);
+        Client client = new Client();
+        client.setAddresses("test@example.com");
+        Template template = new Template("Hello #{name}!");
+        Map<String, String> variables = Map.of("name", "John");
+
+        messenger.sendMessage(client, template, variables);
+
+        verify(mailServer).send(eq("test@example.com"), eq("Hello John!"));
+    }
+
+}
