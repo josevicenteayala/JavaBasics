2e7a1102123e428def0eece89c81deda18d6fc85
Jose Vicente Ayala Luna <jayala@starbucks.com>
2023-06-01 00:23:13 -0500

    Implement test to verify GenerateMessage handles additional variables

--- messenger/src/test/java/com/epam/ld/module2/testing/template/TemplateEngineTest.java
+++ messenger/src/test/java/com/epam/ld/module2/testing/template/TemplateEngineTest.java
@@ -27,4 +27,12 @@ public class TemplateEngineTest {
         assertThrows(MissingValueException.class, () -> templateEngine.generateMessage(template, variables));
     }
 
+    @Test
+    public void testGenerateMessageWithAdditionalVariablesReplacements() throws MissingValueException {
+        TemplateEngine templateEngine = new TemplateEngine();
+        Template template = new Template("Hello, #{name}! You are a #{language} programmer.");
+        Map<String, String> variables = Map.of("name","Jose", "language","Java", "tag","Not used");
+        String message = templateEngine.generateMessage(template, variables);
+        assertEquals("Hello, Jose! You are a Java programmer.", message);
+    }
 }
