88b1e877b9d9d09b73ddf101a402cf97e9449541
Jose Vicente Ayala Luna <jayala@starbucks.com>
2023-05-31 22:33:13 -0500

    Add test to validate extraction of variable names from template text

--- messenger/src/test/java/com/epam/ld/module2/testing/template/TemplateTest.java
+++ messenger/src/test/java/com/epam/ld/module2/testing/template/TemplateTest.java
@@ -2,6 +2,8 @@ package com.epam.ld.module2.testing.template;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class TemplateTest {
@@ -18,4 +20,13 @@ class TemplateTest {
         assertEquals("Hello, world!", template.getText());
     }
 
+    @Test
+    public void testGetVariableNames() {
+        Template template = new Template("Hello, #{name}! You are #{age} years old.");
+        List<String> expectedVariables = new ArrayList<>();
+        expectedVariables.add("name");
+        expectedVariables.add("age");
+        List<String> actualVariables = template.getVariableNames();
+        assertEquals(expectedVariables, actualVariables);
+    }
 }
