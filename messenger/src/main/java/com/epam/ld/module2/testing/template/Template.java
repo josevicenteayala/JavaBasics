fdd06360ea18a457a682558e8b823714095c08db
Jose Vicente Ayala Luna <jayala@starbucks.com>
2023-05-31 22:51:05 -0500

    Refactor metod to extract variable names from template state.

--- messenger/src/main/java/com/epam/ld/module2/testing/template/Template.java
+++ messenger/src/main/java/com/epam/ld/module2/testing/template/Template.java
@@ -1,6 +1,9 @@
 package com.epam.ld.module2.testing.template;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * The type Template.
@@ -18,6 +21,14 @@ public class Template {
     }
 
     public List<String> getVariableNames() {
-        return List.of();
+        List<String> variableNames = new ArrayList<>();
+        Pattern pattern = Pattern.compile("#\\{([\\w\\.]+)}");
+        Matcher matcher = pattern.matcher(text);
+
+        while (matcher.find()) {
+            variableNames.add(matcher.group(1));
+        }
+
+        return variableNames;
     }
 }
