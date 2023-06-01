ff33cfcda36f5f804261c93339aaa421ede9b218
Jose Vicente Ayala Luna <jayala@starbucks.com>
2023-05-31 23:41:26 -0500

    Implement test to validate the message generation

--- messenger/src/test/java/com/epam/ld/module2/testing/template/TemplateEngineTest.java
+++ messenger/src/test/java/com/epam/ld/module2/testing/template/TemplateEngineTest.java
@@ -1,4 +1,18 @@
 package com.epam.ld.module2.testing.template;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.epam.ld.module2.testing.Client;
+import org.junit.jupiter.api.Test;
+
 public class TemplateEngineTest {
+
+    @Test
+    public void testGenerateValidMessage(){
+        TemplateEngine templateEngine = new TemplateEngine();
+        Template template = new Template("Hello, #{name}! You are a #{language} programmer.");
+        String message = templateEngine.generateMessage(template, new Client());
+        assertEquals("Hello, Jose! You are a Java programmer.", message);
+    }
+
 }
