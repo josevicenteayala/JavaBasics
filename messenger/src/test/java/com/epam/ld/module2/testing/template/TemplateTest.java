fd42af05a6403fde4e2a941efcb4800d4ff61607
Jose Vicente Ayala Luna <jayala@starbucks.com>
2023-05-31 21:58:49 -0500

    Add initial fail test to validate the Template creation

--- messenger/src/test/java/com/epam/ld/module2/testing/template/TemplateTest.java
+++ messenger/src/test/java/com/epam/ld/module2/testing/template/TemplateTest.java
@@ -1,7 +1,14 @@
 package com.epam.ld.module2.testing.template;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class TemplateTest {
+
+    @Test
+    public void testConstructor() {
+        fail();
+    }
 
 }
