86019684978260348b458ae1e74d22f4e6a912a8
Jose Vicente Ayala Luna <jayala@starbucks.com>
2023-06-02 09:04:35 -0500

    Refactor: Remove unnecessary logic for sending messages when using the console

--- messenger/src/test/java/com/epam/ld/module2/testing/modes/ConsoleModeTest.java
+++ messenger/src/test/java/com/epam/ld/module2/testing/modes/ConsoleModeTest.java
@@ -1,11 +1,7 @@
 package com.epam.ld.module2.testing.modes;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.mock;
 
-import com.epam.ld.module2.testing.MailServer;
-import com.epam.ld.module2.testing.Messenger;
-import com.epam.ld.module2.testing.template.TemplateEngine;
 import java.util.Map;
 import org.junit.jupiter.api.Test;
 
@@ -14,9 +10,6 @@ class ConsoleModeTest {
     @Test
     public void testPrintMessageUsingConsoleMode() {
         PrintOutput console = new ConsoleMode();
-        TemplateEngine engine = new TemplateEngine();
-        MailServer mailServer = mock(MailServer.class);
-        Messenger messenger = new Messenger(mailServer, engine);
         String input = "Hello #{name}!";
         Map<String, String> variables = Map.of("name", "John");
 
