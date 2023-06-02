d620ed8ac4a7484ee4d0070fb5b7bd195d70ce01
Jose Vicente Ayala Luna <jayala@starbucks.com>
2023-06-02 08:50:14 -0500

    Add test to ensure proper email sending functionality

--- messenger/src/test/java/com/epam/ld/module2/testing/MailServerTest.java
+++ messenger/src/test/java/com/epam/ld/module2/testing/MailServerTest.java
@@ -1,4 +1,20 @@
 package com.epam.ld.module2.testing;
 
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+import org.junit.jupiter.api.Test;
+
 public class MailServerTest {
+
+    @Test
+    public void testSendEmail(){
+        MailServer mailServer = mock(MailServer.class);
+        String mail = "mock@email.com";
+        String messageContent = "Dear mock, hope you are doing well.";
+        mailServer.send(mail, messageContent);
+
+        verify(mailServer).send(mail, messageContent);
+    }
+
 }
