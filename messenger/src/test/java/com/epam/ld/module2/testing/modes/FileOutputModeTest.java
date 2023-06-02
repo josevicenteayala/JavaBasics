834ce1d7ed0df2d88b8e223fa52558508eacbbd4
Jose Vicente Ayala Luna <jayala@starbucks.com>
2023-06-02 08:41:09 -0500

    Refactor:Improve accuracy of File output test

--- messenger/src/test/java/com/epam/ld/module2/testing/modes/FileOutputModeTest.java
+++ messenger/src/test/java/com/epam/ld/module2/testing/modes/FileOutputModeTest.java
@@ -1,16 +1,10 @@
 package com.epam.ld.module2.testing.modes;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.when;
 
-import com.epam.ld.module2.testing.Client;
-import com.epam.ld.module2.testing.MailServer;
-import com.epam.ld.module2.testing.Messenger;
-import com.epam.ld.module2.testing.template.TemplateEngine;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -52,26 +46,10 @@ class FileOutputModeTest {
         doNothing().when(fileOutput).ctrlP();
         when(fileOutput.getOutput()).thenReturn(expectedOutput);
 
-        TemplateEngine engine = new TemplateEngine();
-        MailServer mailServer = mock(MailServer.class);
-        doNothing().when(mailServer).send(any(String.class), any(String.class));
-        Messenger messenger = new Messenger(mailServer, engine);
-        Client client = new Client();
-        client.setAddresses("test@example.com");
-
-        fileOutput.input("/home/joseayala/input.txt").ctrlD();
+        fileOutput.input("input.txt").ctrlD();
         Map<String, String> variables = Map.of("name", "John");
         fileOutput.input(variables).ctrlP();
 
         assertEquals(expectedOutput, fileOutput.getOutput());
-
-        //Read the file created with the response
-        Path pathResponse = Path.of("/home/joseayala/output.txt");
-        try {
-            String output = Files.readString(pathResponse);
-            assertEquals(expectedOutput, output);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
     }
 }
