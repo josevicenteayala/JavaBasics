7baf1c91e821f571c04c88424b19e16d185e7eb3
Jose Vicente Ayala Luna <jayala@starbucks.com>
2023-06-06 14:32:12 -0500

    Add FileOutput test using mocks

--- messenger/src/test/java/com/epam/ld/module2/testing/modes/FileOutputModeTest.java
+++ messenger/src/test/java/com/epam/ld/module2/testing/modes/FileOutputModeTest.java
@@ -3,9 +3,7 @@ package com.epam.ld.module2.testing.modes;
 import static java.util.Arrays.asList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertIterableEquals;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.spy;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -38,10 +36,27 @@ class FileOutputModeTest {
     }
 
     @Test
-    public void testPrintMessageUsingFileModeAndMockingFileBehavior() {
+    public void testPrintMessageUsingFileModeAndSpyingFileBehavior() {
         String expectedOutput = "Hello John!\n";
         PrintOutput fileOutput = spy(FileOutputMode.class);
         doNothing().when(fileOutput).ctrlP();
+        when(fileOutput.getOutput()).thenReturn(expectedOutput);
+
+        fileOutput.input("input.txt").ctrlD();
+        Map<String, String> variables = Map.of("name", "John");
+        fileOutput.input(variables).ctrlP();
+
+        assertEquals(expectedOutput, fileOutput.getOutput());
+    }
+
+    @Test
+    public void testPrintMessageUsingFileModeAndMockingFileBehavior() {
+        String expectedOutput = "Hello John!\n";
+        PrintOutput fileOutput = mock(FileOutputMode.class);
+        doNothing().when(fileOutput).ctrlP();
+        doNothing().when(fileOutput).ctrlD();
+        when(fileOutput.input(anyString())).thenReturn(fileOutput);
+        when(fileOutput.input(any(Map.class))).thenReturn(fileOutput);
         when(fileOutput.getOutput()).thenReturn(expectedOutput);
 
         fileOutput.input("input.txt").ctrlD();
